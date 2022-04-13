80ec6d3392390e69424d93de8b8f28cc9a0fab1b
supalarry <lauris.skraucis@gmail.com>
2022-04-13 11:12:04 +0200

    Get headsups from supabase

--- source/controllers/headsup/list.ts
+++ source/controllers/headsup/list.ts
@@ -1,19 +1,15 @@
 import { NextFunction, Request, Response } from 'express';
 
 import Logger from '../../services/logger';
+import {getHeadsups} from "../../db/supabase";
 
 const NAMESPACE = 'headsup list';
 
-const headsupList = (req: Request, res: Response, next: NextFunction) => {
+const headsupList = async (req: Request, res: Response, next: NextFunction) => {
     Logger.info(NAMESPACE, 'Invoked');
 
-    return res.status(200).json({
-        messages: [
-            {type: 'warning', text: 'Someone spilled coffee on Sentry servers, so Vercel builds are failing'},
-            {type: 'info', text: 'New joiner - let\'s welcome Robert Cecil Martin to the team!'},
-            {type: 'success', text: 'Prisma was updated to the latest version 30.0.0!'},
-        ]
-    });
+    const headsups = await getHeadsups();
+    return res.status(200).json(headsups);
 };
 
 export default { headsupList };
